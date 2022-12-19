9687e30391693dc4cdfc73c3bcf6d6e79bc7a57a
yuna <yuna@MacBook-Air.local>
2022-12-19 16:31:47 +0900

    [add]カテゴリの作成
    各Controller・Viewファイルの追加・作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   mount_uploader :image, ImageUploader
+
+  has_many :categories, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_18_185239) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_19_072620) do
+  create_table "categories", charset: "utf8mb3", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", default: "ユーザー"
     t.string "image"
@@ -25,4 +33,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_18_185239) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "categories", "users"
 end
